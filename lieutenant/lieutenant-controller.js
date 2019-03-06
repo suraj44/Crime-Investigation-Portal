1b98b8c7f5868a3f4f9d0960ed92e96f8cbefc7a
Suraj Singh <suraj1998@gmail.com>
2019-03-06 15:35:38 +0530

    started work on assigning detectives to cases

---
+++ lieutenant/lieutenant-controller.js
@@ -0,0 +1,18 @@
+const model = require('./case-model');
+const login_model = require('../login/login-model');
+var path = require('path');
+
+exports.addDetectivesToCases = function(req,res) {
+    // parse the caseid from request
+    caseid = 0;
+
+    // TODO: parse detective IDs from request and store in list
+
+    list = []
+
+    list.forEach(function(listItem, index){
+        model.assignDetective(casid, listItem,function(result) {
+            ;
+        } ) 
+      });
+} 
